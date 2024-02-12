17c45e0524f40933ba3ee2d77b7cc0e0a26fc972
ryo034 <uni.nashi.034@gmail.com>
2024-02-12 12:10:17 +0900

    fix test file name

--- apps/system/test/api/tests/workspace/create.spec.ts
+++ apps/system/test/api/tests/workspace/create.spec.ts
@@ -4,15 +4,13 @@ import { genAPIClient, getAuthInfo, statefulTest } from "../../scripts"
 
 const client = genAPIClient()
 
-test.describe("Workspace Validation", () => {
+test.describe("Create Workspace", () => {
   test("Workspace can not be created with already exists subdomain", async () => {
     const authInfo = await getAuthInfo("system_account@example.com")
     const hs = authHeaders(authInfo.token)
     const res = await client.POST("/api/v1/workspaces", {
       headers: hs,
-      body: {
-        subdomain: "example"
-      }
+      body: { subdomain: "example" }
     })
     expect(res.response.status).toBe(409)
   })
@@ -21,9 +19,7 @@ test.describe("Workspace Validation", () => {
     const authInfo = await getAuthInfo("system_account@example.com")
     const res = await client.POST("/api/v1/workspaces", {
       headers: authHeaders(authInfo.token),
-      body: {
-        subdomain: "test-example-subdomain"
-      }
+      body: { subdomain: "test-example-subdomain" }
     })
     expect(res.response.status).toBe(201)
     expect(res.error).toBeUndefined()
