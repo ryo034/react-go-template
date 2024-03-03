1d87078de32146d5d54937b0a8d64fe4b36a02bd
ryo034 <uni.nashi.034@gmail.com>
2024-03-04 08:21:22 +0900

    fix theme init

--- apps/system/client/src/layout/theme/index.tsx
+++ apps/system/client/src/layout/theme/index.tsx
@@ -1,44 +1,13 @@
-import { useContext, useEffect, useLayoutEffect } from "react"
+import { useContext, useLayoutEffect } from "react"
 import { Outlet } from "react-router-dom"
 import { ContainerContext } from "~/infrastructure/injector/context"
 
 export const ThemeLayout = () => {
-  const { store, controller } = useContext(ContainerContext)
-  const isDark = store.theme((state) => state.isDark)
-
-  const _onChangeTheme = () => {
-    controller.theme.toggle(!isDark)
-  }
+  const { controller } = useContext(ContainerContext)
 
   useLayoutEffect(() => {
     controller.theme.init()
   }, [])
 
-  useEffect(() => {
-    store.theme.subscribe((state) => {
-      document.documentElement.classList.toggle("dark", state.isDark)
-    })
-  }, [])
-
-  return (
-    <>
-      {/* <header className="fixed top-0 w-full bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-        <nav className="">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" className="flex items-center text-xl space-x-2 font-semibold text-gray-900 dark:text-white">
-              <img className="w-8 h-8" src={sandboxImage} alt="logo" />
-              <span>Sandbox on Vite</span>
-            </a>
-            <label className="flex items-center space-x-2 cursor-pointer">
-              <Switch id="switchTheme" checked={isDark} onCheckedChange={onChangeTheme} />
-              <Label htmlFor="switchTheme" className="cursor-pointer">
-                Switch Theme
-              </Label>
-            </label>
-          </div>
-        </nav>
-      </header> */}
-      <Outlet />
-    </>
-  )
+  return <Outlet />
 }
