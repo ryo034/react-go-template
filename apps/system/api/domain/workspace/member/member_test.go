7515ea656ea79833fba8e1370a64ad8dd9496ed0
ryo034 <uni.nashi.034@gmail.com>
2024-03-12 15:51:05 +0900

    fix go unit test

--- apps/system/api/domain/workspace/member/member_test.go
+++ apps/system/api/domain/workspace/member/member_test.go
@@ -11,10 +11,11 @@ import (
 
 func TestMember_UpdateRole_OK(t *testing.T) {
 	type fields struct {
-		id      ID
-		u       *user.User
-		profile Profile
-		role    Role
+		id               ID
+		u                *user.User
+		profile          Profile
+		role             Role
+		membershipStatus MembershipStatus
 	}
 	type args struct {
 		role Role
@@ -28,32 +29,32 @@ func TestMember_UpdateRole_OK(t *testing.T) {
 	}{
 		{
 			name:   "Admin can update to Member Role",
-			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin},
+			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin, membershipStatus: MembershipStatusActive},
 			args:   args{RoleMember},
-			want:   &Member{ID{}, &user.User{}, Profile{}, RoleMember},
+			want:   &Member{ID{}, &user.User{}, Profile{}, RoleMember, MembershipStatusActive},
 		},
 		{
 			name:   "Admin can update to Guest Role",
-			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin},
+			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin, membershipStatus: MembershipStatusActive},
 			args:   args{RoleGuest},
-			want:   &Member{ID{}, &user.User{}, Profile{}, RoleGuest},
+			want:   &Member{ID{}, &user.User{}, Profile{}, RoleGuest, MembershipStatusActive},
 		},
 		{
 			name:   "Member can update to Admin Role",
-			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleMember},
+			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleMember, membershipStatus: MembershipStatusActive},
 			args:   args{RoleAdmin},
-			want:   &Member{ID{}, &user.User{}, Profile{}, RoleAdmin},
+			want:   &Member{ID{}, &user.User{}, Profile{}, RoleAdmin, MembershipStatusActive},
 		},
 		{
 			name:   "Member can update to Guest Role",
-			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleMember},
+			fields: fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleMember, membershipStatus: MembershipStatusActive},
 			args:   args{RoleGuest},
-			want:   &Member{ID{}, &user.User{}, Profile{}, RoleGuest},
+			want:   &Member{ID{}, &user.User{}, Profile{}, RoleGuest, MembershipStatusActive},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &Member{tt.fields.id, tt.fields.u, tt.fields.profile, tt.fields.role}
+			w := &Member{tt.fields.id, tt.fields.u, tt.fields.profile, tt.fields.role, tt.fields.membershipStatus}
 			got, err := w.UpdateRole(tt.args.role)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UpdateRole() error = %v, wantErr %v", err, tt.wantErr)
@@ -68,10 +69,11 @@ func TestMember_UpdateRole_OK(t *testing.T) {
 
 func TestMember_UpdateRole_Validate(t *testing.T) {
 	type fields struct {
-		id      ID
-		u       *user.User
-		profile Profile
-		role    Role
+		id               ID
+		u                *user.User
+		profile          Profile
+		role             Role
+		membershipStatus MembershipStatus
 	}
 
 	tests := []struct {
@@ -81,39 +83,39 @@ func TestMember_UpdateRole_Validate(t *testing.T) {
 		wantErr error
 	}{
 		{"Can not update to Same Role",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleAdmin, membershipStatus: MembershipStatusActive},
 			RoleAdmin,
 			domainErr.NewBadRequest("the role is already the same"),
 		},
 		{"Can not update to Owner Role if Owner",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner, membershipStatus: MembershipStatusActive},
 			RoleOwner,
 			domainErr.NewForbidden("cannot change the role to owner"),
 		},
 		{"Can not update to Admin Role if Owner",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner, membershipStatus: MembershipStatusActive},
 			RoleAdmin,
 			domainErr.NewForbidden("cannot change the role"),
 		},
 		{"Can not update to Admin Role if Owner",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner, membershipStatus: MembershipStatusActive},
 			RoleGuest,
 			domainErr.NewForbidden("cannot change the role"),
 		},
 		{"Can not update to Member Role if Member",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner, membershipStatus: MembershipStatusActive},
 			RoleMember,
 			domainErr.NewForbidden("cannot change the role"),
 		},
 		{"Can not update to Guest Role if Guest",
-			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner},
+			fields{id: ID{}, u: &user.User{}, profile: Profile{}, role: RoleOwner, membershipStatus: MembershipStatusActive},
 			RoleGuest,
 			domainErr.NewForbidden("cannot change the role"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			w := &Member{tt.fields.id, tt.fields.u, tt.fields.profile, tt.fields.role}
+			w := &Member{tt.fields.id, tt.fields.u, tt.fields.profile, tt.fields.role, tt.fields.membershipStatus}
 			_, err := w.UpdateRole(tt.value)
 			if err == nil {
 				t.Errorf("UpdateRole() error = %v, wantErr %v", err, tt.wantErr)
